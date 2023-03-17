7a2170326203e948410e939f31af03edfe6319e8
Brooke Jacobs <81982652+testonamonaco@users.noreply.github.com>
2023-03-17 12:23:37 +0100

    Added contents and sections

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'GDCGDCGDGCGD'
+copyright = '2023, GEOAP'
+author = 'Jorge Qirdahi'
 
 release = '0.1'
 version = '0.1.0'
